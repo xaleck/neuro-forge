370b863b18761c4ab1e044c06f53cfd0a055c77a
xaleck <xaleck@proton.me>
2025-05-08 16:45:05 +0600

    init && setup

---
+++ neuro-forge_back/src/main/java/com/aitstudgroup/ala_ata/demo/NeuroForgeApplication.java
@@ -0,0 +1,13 @@
+package com.aitstudgroup.ala_ata.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroForgeApplication.class, args);
+	}
+
+}
---
+++ neuro-forge_back/src/test/java/com/aitstudgroup/ala_ata/demo/NeuroForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitstudgroup.ala_ata.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
